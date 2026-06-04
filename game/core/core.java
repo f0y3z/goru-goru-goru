22d44cc04b4b911ca68265ea3df0f5368efac230
f0y3z <foyezz@proton.me>
2026-06-04 23:15:03 +0600

    goru laf dey

--- game/core/core.java
+++ game/core/core.java
@@ -7,17 +7,19 @@ import javafx.scene.image.ImageView;//this is for rendering image
 import javafx.geometry.Rectangle2D;//to calculate position
 import javafx.scene.input.KeyCode;//to listen for keyperess
 
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 
 public class core extends Application{
 	//variables to control the viewport of the player sprite
-	private	int frameHeight=256;
-	private	int frameWidth=256;
+	private	int frameHeight=216;
+	private	int frameWidth=216;
 	private int totalCols=4; 	
 	private	int col=0;
 	private	int row =0;
-
+	private boolean running= false;
 	public void start(Stage primaryStage){
 		//pane holds all othe nodes
 		Pane root=new Pane();
@@ -25,6 +27,7 @@ public class core extends Application{
 		//rendering background
 		Image background = new Image("file:background.png");
 		ImageView back= new ImageView(background);
+		ImageView back2= new ImageView(background);
 		back.setFitWidth(960);
 		back.setFitHeight(540);
 		back.setSmooth(false);
@@ -46,37 +49,70 @@ public class core extends Application{
 
 
 			
-		//animation and movement
+		//Controls
         	scene.setOnKeyPressed(event -> {
+			if (running) {return;}//if animaiton is still running dont register keypress.
+
             		KeyCode key = event.getCode();
-            		double speed = 10.0;
             		if (key == KeyCode.RIGHT) {
-                		player.setTranslateX(player.getTranslateX() + speed);
-                		row = 0; // row facing right
-                		cycleAnimation();
-                		updateViewport(player);
+                		row = 0;
+				player.setScaleX(1);
+                		cycleAnimation(player);
             		} 
             		else if (key == KeyCode.LEFT) {
-                		player.setTranslateX(player.getTranslateX() - speed);
-                		row = 1; // this will be changed later for multidirectional movement 
-                		cycleAnimation();
-                		updateViewport(player);
+                		row = 0; 
+				player.setScaleX(-1);
+                		cycleAnimation(player);
             		}
+			else if (key == KeyCode.UP){
+				row = 2;
+				cycleAnimation2(player);
+			}
+			else if(key == KeyCode.DOWN){
+				row =3;
+				cycleAnimation(player);
+			}
         	});
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	//frame calculation
-    	private void cycleAnimation() {
-        	col = (col + 1) % totalCols; //0 1 2 3 0 1 2 3
-    	}
-    	//updatting viewport
-    	private void updateViewport(ImageView iv) {
-        	Rectangle2D viewport = new Rectangle2D(col * frameWidth, row * frameHeight, frameWidth, frameHeight);
-        	iv.setViewport(viewport);
-    	}			
+	//Sprite display
+	private void cycleAnimation(ImageView iv){
+		col =0;
+		running=true;//this stops registering keystrokes while animation is still palyeing
+		Timeline timeline = new Timeline(
+			new KeyFrame(Duration.millis(150), event ->{
+				Rectangle2D viewport = new Rectangle2D(col*frameWidth, row*frameHeight, frameWidth, frameHeight);
+				iv.setViewport(viewport);
+				col++;
+				})
+			);
+		timeline.setCycleCount(4);
+		timeline.setOnFinished(event -> running =false);
+		timeline.play();
+	}//spirte display fix
+	private void cycleAnimation2(ImageView iv){
+                col =0;
+                running=true;//this stops registering keystrokes while animation is still palyeing
+                Timeline timeline = new Timeline(
+                        new KeyFrame(Duration.millis(150), event ->{
+                                Rectangle2D viewport = new Rectangle2D(col*frameWidth, row*frameHeight, frameWidth, frameHeight);
+                                iv.setViewport(viewport);
+                                col++;
+                                })
+                        );
+		//fixiing slop sprite defect with code
+                timeline.setCycleCount(4);
+		timeline.setOnFinished(event -> {
+			Rectangle2D firstframe = new Rectangle2D(0,row*frameHeight, frameWidth,frameHeight);
+			iv.setViewport(firstframe);
+			running=false;
+		});
+                timeline.play();
+	}
+	
 	public static void main(String[] args){
 		launch(args);
 	}
